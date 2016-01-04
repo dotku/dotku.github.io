070f68c57910f8e816d894a4390918dc0e73fff5
Jay Lin <weijingjaylin@gmail.com>
2016-01-04 01:29:26 -0800

    update librarys

---
+++ tech/javascript/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>JavaScript</title>
+		<link type="text/css" rel="stylesheet" href="/css/general.css"/>
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+		<!-- Latest compiled and minified JavaScript -->
+		
+		<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	</head>
+	<body class="container mw800">
+		<h1>JavaScript</h1>
+        <h2>Created: 07/20/2015</h2>
+		<p>JavaScript 最早是 NetScape 开发的（现在 Mozilla 非营利组织），在经历了一段的风风雨雨之后，现在做为 ISO 标准的脚本语言，运行于各大主流浏览器。由于 JavaScript 在服务器上的局限性，有人用 Google 的 JavaScript V8 开发了 <a href="nodejs">NodeJS</a>，一股 JavaScript 革命烽烟四起，之后，JavaScript 也变得非常复杂了，不再只是简单的修改一下颜色，位置，做点验证，还需要模块开发等等。现在的 JavaScript 库都有这些（有时候实在分不清是 NodeJS 还是 JavaScript，不用不知道了）</p>
+        <h2>资源</h2>
+		<ul>
+			<li>SeaJS 
+				<br> 这是一款有国人开发的，基于 CMD 规范的 JavaScript 模块管理工具 https://github.com/seajs
+				<br> 例子: https://ghsky.com/2011/05/seajs-first-view.html
+			</li>
+		</ul>
+		<h2>软件</h2>
+		<ul>
+			<li>WebStorm <br>前有 PHPStorm，这里有 WebStorm，支持正版哟 http://www.jetbrains.com/webstorm/</li>
+		</ul>
+        <h2>Updated: 12/25/2015</h2>
+        <p>最近在研究技术，对 JavaScript 有了更多的了解，硅谷这边的前端已经非常工程化了，通过 NodeJS 迅速崛起了一堆的前端技术，比如类似 GEM 的 NPM 包裹管理器，Bower 前端包裹管理器，一堆的前端框架雨后春笋般崛起，甚至现在成为了找工作的标配。</p>
+        <p>必修的前端应用框架有这些:</p>
+        <ul>
+            <li>AngularJS (2009 年)
+                <p>Google 的 AngularJS 成为业界神级标配</p>
+            </li>
+            <li>Backbone (2014 年)
+                <p>CoffeeScript 的开发者 Jeremy Ashkenas 所开发的框架，由于 CoffeeScript (2009 年) 在业内很有知名度，甚至微软都推出了 TypeScript (2012 年) 山寨他们，可见 Jeremy 的魅力。</p>
+                <p>Backbone 比较类似 AngularJS，不过我觉得比 AngularJS 比较难懂。</p>
+            </li>
+            <li>React (2013 年)
+                <p>React 是 Facebook 的 Jordan Walke 编写的脚本库，听说具有 Mobile Web 兼得的功能，还没有试过</p>
+            </li>
+        </ul>
+        <p>必修的前端框架</p>
+        <ul>
+            <li>BootStrap
+                <p>BootStrap 是 Twitter 公司开发的前端框架，不仅仅只是前面提到的应用的框架，包含了 CSS 的内容，所以是 JS+CS 的框架 (由于语义化的关系，HTML 无法定制，所以基本上改不了了，虽然 Facebook 之前尝试用什么 FBTAG 之类的，不过似乎被淘汰了...)，实际上也就是 BootCSS + jQuery 的感觉，基本上 BootStrap 最大的贡献就是在 CSS 上，而 JS 部分就是 jQuery 的内容。</p>
+            </li>
+        </ul>
+	</body>
+</html>
